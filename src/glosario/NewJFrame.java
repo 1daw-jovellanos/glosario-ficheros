c71901810b5788d7ccda2612a75a6a3c7a46606e
Victor fernandez <victor.fernandezgil@educa.madrid.org>
2018-04-24 17:37:12 +0200

    cambio nombre paquete

--- src/glosario/NewJFrame.java
+++ src/glosario/NewJFrame.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package diccionario;
+package glosario;
 
 import java.awt.Dialog;
 import javax.swing.JOptionPane;
